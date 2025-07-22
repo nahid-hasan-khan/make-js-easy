d78dcb3ee9f8e2980be365220401fb6196dba912
nahid-hasan-khan <nahidhasankhan.dev@gmail.com>
2025-07-22 17:57:29 +0600

    javascript practice complete

---
+++ array/script.js
@@ -0,0 +1,11 @@
+console.log("hello array");
+
+const arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);
+console.log((arr[2] = 252));
+console.log(arr.length);
+console.log(arr[3]);
+console.log(arr.join("and"));
+console.log(arr.pop());
+console.log(arr.push(25));
+  
---
+++ excersise-2/script.js
@@ -0,0 +1,26 @@
+/*
+create a business name generator by combining list of adjective and shop name and another word
+
+adjectives:
+crazy
+amazing
+fire
+
+shop name:
+Engine
+foods
+germents
+
+another word:
+bros
+limited
+hub
+*/
+
+const adjective={
+    
+}
+
+const nameGenerator =(obj1,obj2,obj3)=>{
+
+}
