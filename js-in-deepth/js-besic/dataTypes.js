1f2f153922310f7f8e44032b4cc17cc43618be4e
nahid-hasan-khan <nahidhasankhan.dev@gmail.com>
2025-07-27 17:13:35 +0600

    datatypes complete

---
+++ js-in-deepth/js-besic/dataTypes.js
@@ -0,0 +1,14 @@
+// "use Strict" eta babohar korle sob javascript er new version hisabe treet korbe
+
+let name="Nahid"; //string type
+let number = 15454554; // Number type
+let isLogedin=true; // boolean type 
+let state; //undefined
+let pack = null; //null
+// symbol
+let boy={
+    name:"nahid", //object
+    age:28
+}
+
+console.log(typeof name); //use for defined datatype
---
+++ js-in-deepth/js-besic/first.js
@@ -0,0 +1,15 @@
+console.log("Nahid");
+
+// .txt and /js./.py/ esob file ex dipend kore compailar ba installed invaronment zemon node era bujhte pare eta ki fail ar er ki wyarrder kaj ki
+const accountId = 144553;
+let accountEmail="nahidkhan@gmail.com";
+var accountPassword="12345hgt"
+accountCity="Jessore"
+let accountState;
+/*majhe majhe javascript let var const na likhle o sudhu na likh;ei setake var baniye dey
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
+
