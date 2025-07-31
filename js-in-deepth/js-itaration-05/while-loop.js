2d6108daa4e9021dfeeff6c84b70e42ddf87b273
nahid-hasan-khan <nahidhasankhan.dev@gmail.com>
2025-07-31 16:43:38 +0600

    while and do while loop complete

---
+++ js-in-deepth/js-itaration-05/while-loop.js
@@ -0,0 +1,24 @@
+//while loop
+
+let index = 0;
+while (index <= 10) {
+  console.log(`value of index is ${index}`);
+  index += 5;
+}
+
+// while loop opration of an array
+
+let myArr = ["kasem", "Robin", "Sobuj", "Sojib"];
+let i = 0;
+while (i < myArr.length) {
+  console.log(`friend is mr${myArr[i]}`);
+  i++;
+}
+
+//do while loop
+let score = 1;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 500);
